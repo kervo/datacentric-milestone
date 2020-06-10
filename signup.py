7f0d93e904e3081bc41688f0e7077a1c085d91ab
kervo <kervinarias@gmail.com>
2020-06-10 17:11:25 +0000

    doc: sign up form

---
+++ signup.py
@@ -0,0 +1,12 @@
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators
+from flask_wtf import Form
+
+class signUpForm(Form):
+    name_first = StringField('User Name', [validators.DataRequired()])
+    email = StringField('Email Address', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    submit = SubmitField('Sign Up')
