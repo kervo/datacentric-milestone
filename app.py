0649cdd91883b3e988c1cfafcf362f2f98779f10
kervo <kervinarias@gmail.com>
2020-06-11 20:16:10 +0000

    signup failed

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 import os
 import pymongo
 from flask import Flask, redirect, url_for, render_template, request
-from signup import signUpForm
-from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators
-from flask_wtf import Form
+from register import signUpForm
 
 from os import path
 if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
+app.config.from_mapping(
+    SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
-
 
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
 
-
-@app.route('/signup', methods=['GET', 'POST'])
-def registration():
+@app.route('/signup')
+def signup():
     form = signUpForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
-        return 'Your registration is complete!'
-    return render_template('signup.html', form=form)
-
+        return 'We confirm your registration!'
+    return render_template("signup.html", form=form)
 
 @app.route('/contact')
 def contact():
@@ -41,4 +36,3 @@ def contact():
 
 if __name__ == '__main__':
     app.run()
-
