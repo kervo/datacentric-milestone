c73d605f682080f928c50cf1676b5b11ac152363
kervo <kervinarias@gmail.com>
2020-06-18 07:17:36 +0000

    user login function started

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
 import os
-from flask import Flask, app, redirect, render_template, request, url_for
-from register import signUpForm, RegistrationForm
+from flask import Flask, app, redirect, render_template, request, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf.form import FlaskForm
 from flask_pymongo import PyMongo
 from forms import *
-from flask.helpers import flash
-from virtualenv import session
+from bson.objectid import ObjectId
+
 
 from os import path
+from flask.helpers import flash
 if path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", 'mongodb://localhost')
+app.config['SECRET_KEY'] = os.urandom(32)
 # Variables for database
 mongo = PyMongo(app)
 users_files = mongo.db.usersfiles
+
+'''MongoDB Variables'''
+user = mongo.db.user
+recipe = mongo.db.recipe
+meal = mongo.db.dish_type
+
 
 @app.route('/')
 def index():
     return render_template("index.html", title='Wondercook')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", title='Log In')
+    form = LoginForm()
+    if form.validate_on_submit():
+        users = user
+        registered_user = users.find_one({'username': request.form['username']})
+
+        if registered_user:
+            if checked_password_hash(registered_user['password'], request.form['password']):
+                session['username'] = request.form['username']
+                flash('Welcome to WonderCook')
+                return redirect(url_for('dashboard'))
+            
+            else:
+                flash('Wrong details, please try again')
+                return redirect(url_for('login'))
+        
+        else:
+            flash('This user does not exist, type your details again')
+            return redirect(url_for('login'))
+    return render_template("login.html", form=form, title='Log In')
 
 @app.route('/dashboard')
 def dashboard():
